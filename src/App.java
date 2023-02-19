ab4f6b94a4cf3fa3333c29607d7ca6c8e7339843
ikram chouider <li_chouider@esi.dz>
2023-02-19 11:49:40 +0100

    works

--- src/App.java
+++ src/App.java
@@ -49,7 +49,7 @@ public class App {
         serviceESI.AddSeance(TD1);
         serviceESI.AddSeance(TPTD1);
         serviceESI.AddSeance(TPTD2);
-        // Deux_CPI.Afficher();
+        Deux_CPI.Afficher();
         // serviceESI.Afficher(POO);
         // serviceESI.Afficher();
         // serviceESI.Afficher(e3);
